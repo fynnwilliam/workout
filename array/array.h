78de83a3b4f5cd8007b45f37940e5dd220c40996
fynnwilliam <fynnwilliam@gmail.com>
2021-08-30 09:27:23 -0400

    initial array implementation

---
+++ array/array.h
@@ -0,0 +1,22 @@
+class array
+{
+private:
+    int* arr_{nullptr};
+    int  size_{};
+    
+public:
+    array() = default;
+    
+    explicit array(int size)
+    {
+        if (size > 0)
+        {
+            arr_ = new int[size]{};
+            size_ = size;
+        }
+    }
+    
+    inline int size() const { return size_; }
+    
+    inline bool empty() const { return size_ == 0; }
+};
