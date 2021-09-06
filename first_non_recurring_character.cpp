13036858280973ec42186e9beeaf88d89724f6d9
fynnwilliam <fynnwilliam@gmail.com>
2021-09-05 20:42:47 -0400

    solved the first non recurring character

---
+++ first_non_recurring_character.cpp
@@ -0,0 +1,28 @@
+#include <unordered_map>
+#include <iostream>
+#include <string>
+
+char first_non_recurring_char(std::string const& s)
+{
+    std::unordered_map<char, int> m;
+    
+    for (char const& c : s)
+        ++m[c];
+        
+    for (char const& c : s)
+        if (m[c] == 1) return c;
+        
+    return ' ';
+}
+
+std::string declare(char const& c)
+{
+    return c == ' ' ? "none found" : std::string{c};
+}
+
+int main()
+{
+    char c = first_non_recurring_char(std::string{"character"});
+    
+    std::cout << declare(c) << std::endl;
+}
