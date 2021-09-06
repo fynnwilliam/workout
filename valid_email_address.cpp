3d659483fcedef90a5e0e40c6d11cafbf9e57d08
fynnwilliam <fynnwilliam@gmail.com>
2021-09-06 13:23:07 -0400

    validate email addresses

---
+++ valid_email_address.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <regex>
+
+bool valid_email(std::string const& item)
+{
+    std::regex pattern{"(\\w+)(\\.|_)?(\\w+)@(\\w+)(\\.(\\w+))+"};
+    return std::regex_match(item, pattern);
+}
+
+int main()
+{
+    std::cout << std::boolalpha << valid_email("example@email.com") << '\n';
+}
