5643b6221551399386648e6c6cb7cb665ab7652b
fynnwilliam <fynnwilliam@gmail.com>
2021-09-13 15:03:56 -0400

    validating mac addresses with regex

---
+++ valid_mac_address.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <regex>
+
+bool mac_address(std::string item)
+{
+    std::regex pattern{"((([a-f]|\\d){2}\\:){5}([a-f]|\\d){2})|((([A-F]|\\d){2}\\:){5}([A-F]|\\d){2})"};
+    return std::regex_match(item, pattern);
+}
+
+int main()
+{
+    std::cout << std::boolalpha << mac_address("2a:27:c7:b7:3d:17") << '\n';
+}
