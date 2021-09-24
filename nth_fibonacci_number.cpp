335ec0afbd1dbaccf2ddc563cbb74a5850d9d20f
fynnwilliam <fynnwilliam@gmail.com>
2021-09-24 18:12:51 -0400

    using memoization to help compute larger nth values, faster

--- nth_fibonacci_number.cpp
+++ nth_fibonacci_number.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <unordered_map>
+#include <cassert>
 
-int nth_fibonacci(int n)
+unsigned long nth_fibonacci(int n)
 {
-    return n <= 0 ? 0 :
-           n == 1 ? 1 : nth_fibonacci(n - 1) + nth_fibonacci(n - 2);
+    static std::unordered_map<int, unsigned long> m;
+    
+    if (m.count(n)) return m[n];
+    if (n <= 0)     return 0;
+    if (n == 1)     return 1;
+
+    m[n] = nth_fibonacci(n - 1) + nth_fibonacci(n - 2);
+    
+    return m[n];
 }
 
 int main()
 {
-    std::cout << nth_fibonacci(40) << std::endl;
+    std::cout << nth_fibonacci(10'000) << std::endl;
 }
