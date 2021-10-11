83c72ec8ba993a44ea7b1d21350b67906226aea2
fynnwilliam <fynnwilliam@gmail.com>
2021-10-10 21:43:31 -0400

    updated approach for first_non_recurring_character.cpp

--- first_non_recurring_character.cpp
+++ first_non_recurring_character.cpp
@@ -1,17 +1,14 @@
-#include <unordered_map>
 #include <iostream>
 #include <string>
 
 char first_non_recurring_char(std::string const& s)
 {
-    std::unordered_map<char, int> m;
+    for (std::size_t i{}; i < s.size(); ++i)
+    {
+        if (s.find_last_of(s[i]) == i)
+            return s[i];
+    }
     
-    for (char const& c : s)
-        ++m[c];
-        
-    for (char const& c : s)
-        if (m[c] == 1) return c;
-        
     return ' ';
 }
 
@@ -22,7 +19,7 @@ std::string declare(char const& c)
 
 int main()
 {
-    char c = first_non_recurring_char(std::string{"character"});
+    char c =  first_non_recurring_char(std::string{"character"});
     
     std::cout << declare(c) << std::endl;
 }
