5e3db03b489f8a7e807f8b16fa5eac396b85abc1
fynnwilliam <fynnwilliam@gmail.com>
2022-01-01 22:05:45 -0500

    updated call to insertion_sort(T&, U...)

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -2,7 +2,6 @@
 #include <iterator>
 #include <cassert>
 #include <vector>
-#include <iostream>
 
 template <typename T>
     requires requires(T x, typename T::value_type value)
@@ -27,16 +26,6 @@ int main()
 {
     std::vector<int>  numbers{1, 3, 4, 6, 7, 8, 10};
 
-    insertion_sort(numbers, 2);
-    insertion_sort(numbers, 0);
-    insertion_sort(numbers, 9);
-    
+    insertion_sort(numbers, 2, 0, 9);
     assert((numbers == std::vector{0, 1, 2, 3, 4, 6, 7, 8, 9, 10}));
-    
-    std::vector<int> v{1, 3, 9, 10, 11};
-    insertion_sort(v, 2, 10, 20, 4, 11, 7, 5, 100);
-    
-    for (int elem : v)
-        std::cout << elem << ' ';
-    std::cout << '\n';
 }
