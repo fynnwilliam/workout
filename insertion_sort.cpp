528eaa66d0475444a2475f1cc379673758d12fbe
fynnwilliam <fynnwilliam@gmail.com>
2022-01-01 22:02:34 -0500

    update insertion_sort with a parameter pack

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -2,14 +2,25 @@
 #include <iterator>
 #include <cassert>
 #include <vector>
+#include <iostream>
 
 template <typename T>
     requires requires(T x, typename T::value_type value)
         {{ x.begin() } -> std::forward_iterator; x.insert(x.begin(), value);}
-void insertion_sort(T& container, typename T::value_type value)
+void _insertion_sort(T& container, typename T::value_type value)
 {
     auto itr{std::ranges::upper_bound(container, value)};
     container.insert(itr, value);
+}
+
+template <typename T, typename... U>
+    requires requires(T x, typename T::value_type value)
+        {{ x.begin() } -> std::forward_iterator; x.insert(x.begin(), value);}
+    && (std::same_as<typename T::value_type, U> && ...)
+void insertion_sort(T& container, U... values)
+{
+    for (auto const& value : {values...})
+        _insertion_sort(container, value);
 }
 
 int main()
@@ -21,4 +32,11 @@ int main()
     insertion_sort(numbers, 9);
     
     assert((numbers == std::vector{0, 1, 2, 3, 4, 6, 7, 8, 9, 10}));
+    
+    std::vector<int> v{1, 3, 9, 10, 11};
+    insertion_sort(v, 2, 10, 20, 4, 11, 7, 5, 100);
+    
+    for (int elem : v)
+        std::cout << elem << ' ';
+    std::cout << '\n';
 }
