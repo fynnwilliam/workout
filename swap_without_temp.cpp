2fb90bf3c75dd3cc6231782d4473bfaf1c258504
fynnwilliam <fynnwilliam@gmail.com>
2022-01-03 06:40:29 -0500

    updated 'swap_s' function

--- swap_without_temp.cpp
+++ swap_without_temp.cpp
@@ -4,19 +4,27 @@
 
 template <typename T>
     requires std::integral<T> || std::floating_point<T>
-void swap_without_temp(T& a, T& b)
+void swap_n(T& a, T& b)
 {
     a += b;
     b = a - b;
     a -= b;
 }
 
-template <typename T>
-void swap_s(T& a, T& b)
+void swap_s(std::string& a, std::string& b)
 {
     a.append(b);
     b = a.substr(0, a.size() - b.size());
     a = a.substr(b.size());
+}
+
+template <typename T>
+void swap_without_temp(T& a, T& b)
+{
+    if constexpr (std::same_as<T, std::string>)
+        return swap_s(a, b);
+    else
+        swap_n(a, b);
 }
 
 int main()
@@ -29,4 +37,7 @@ int main()
     
     std::string s1{"world"};
     std::string s2{"hello"};
+    
+    swap_without_temp(s1, s2);
+    assert(s1 == "hello" && s2 == "world");
 }
