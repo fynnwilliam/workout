a7b68b3f37e45ed195054ae6fef3a8eb47c0f41d
fynnwilliam <fynnwilliam@gmail.com>
2022-01-20 18:38:26 -0500

    updated linked_list<T>::push_back using some helper functions

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -9,17 +9,7 @@ std::size_t linked_list<T>::size() const noexcept
 template <typename T>
 void linked_list<T>::push_back(T const& item) noexcept
 {
-    if (size())
-    {
-        tail_->next = std::make_unique<node>(node{item});
-        tail_->next->previous = tail_;
-        tail_ = tail_->next.get();
-    }
-    else
-    {
-        head_ = std::make_unique<node>(node{item});
-        tail_ = head_.get();
-    }
+    size() ? _push_back(item) : initial_push(item);
     ++size;
 }
 
