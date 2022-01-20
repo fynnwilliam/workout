66afe80695919da554d8933be619c04f0e4814b5
fynnwilliam <fynnwilliam@gmail.com>
2022-01-20 17:40:34 -0500

    increasing linke_list<T>::size_ after pushing...

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -9,7 +9,7 @@ std::size_t linked_list<T>::size() const noexcept
 template <typename T>
 void linked_list<T>::push_back(T const& item) noexcept
 {
-    if (size_++)
+    if (size_)
     {
         tail_->next = std::make_unique<node>(node{item});
         tail_->next->previous = tail_;
@@ -20,4 +20,5 @@ void linked_list<T>::push_back(T const& item) noexcept
         head_ = std::make_unique<node>(node{item});
         tail_ = head_.get();
     }
+    ++size;
 }
