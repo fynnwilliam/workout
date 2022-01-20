2997170e1587d4715bb9a487a55f4ec41ad0f323
fynnwilliam <fynnwilliam@gmail.com>
2022-01-20 17:03:15 -0500

    implemented linked_list<T>::push_back

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -5,3 +5,18 @@ std::size_t linked_list<T>::size() const noexcept
 {
     return size_;
 }
+
+template <typename T>
+void linked_list<T>::push_back(T const& item) noexcept
+{
+    if (size_)
+    {
+        tail_->next = std::make_unique<node>(node{item});
+        tail_ = tail_->next.get();
+    }
+    else
+    {
+        head_ = std::make_unique<node>(node{item});
+        tail_ = head_.get();
+    }
+}
