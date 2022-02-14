479df3ffd5ccf0cf675422e8b7f47148a0b1c8ed
fynnwilliam <fynnwilliam@gmail.com>
2022-02-14 04:37:46 -0500

    accepting 'word' as const&

--- dynamic_programming/tabulation/count_construct.cpp
+++ dynamic_programming/tabulation/count_construct.cpp
@@ -9,7 +9,7 @@ int count_construct(std::string s, std::vector<std::string> word_bank)
     
     for (std::size_t i{}; i < table.size(); ++i)
     {
-        for (auto& word : word_bank)
+        for (auto const& word : word_bank)
         {
             if (table[i] && s.substr(i).find(word) == 0)
                 table[i + word.size()] += table[i];
@@ -23,5 +23,5 @@ int main()
 {
     std::cout << std::boolalpha
               << count_construct("purple", {"purp", "p", "ur", "le", "purpl"})
-              << '\n';
+              << std::endl;
 }
