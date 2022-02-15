fc14fbb4227aff2e2a0ae02de79e1b0f5367fa1f
fynnwilliam <fynnwilliam@gmail.com>
2022-02-15 12:30:45 -0500

    renamed palindrome(std::string_view) -> is_palindrome(std::string_view)

--- recursion/is_palindrome.cpp
+++ recursion/is_palindrome.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string_view>
 
-bool palindrome(std::string_view sv)
+bool is_palindrome(std::string_view sv)
 {
     return sv.size() < 2             ? true
-           : sv.front() == sv.back() ? palindrome(sv.substr(1, sv.size() - 2))
+           : sv.front() == sv.back() ? is_palindrome(sv.substr(1, sv.size() - 2))
                                      : false;
 }
 
 int main()
 {
-    std::cout << std::boolalpha << palindrome("kayak") << std::endl;
+    std::cout << std::boolalpha << is_palindrome("kayak") << std::endl;
 }
