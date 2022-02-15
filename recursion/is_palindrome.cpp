9a206d4592848009d5fabb3807407551d659954c
fynnwilliam <fynnwilliam@gmail.com>
2022-02-15 12:32:18 -0500

    marked is_palindrome as constexpr

--- recursion/is_palindrome.cpp
+++ recursion/is_palindrome.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string_view>
 
-bool is_palindrome(std::string_view sv)
+constexpr bool is_palindrome(std::string_view sv)
 {
     return sv.size() < 2             ? true
            : sv.front() == sv.back() ? is_palindrome(sv.substr(1, sv.size() - 2))
