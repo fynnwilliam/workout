47e6c0980a71a345d20f9c3989f6b5eb8f6d5222
fynnwilliam <fynnwilliam@gmail.com>
2022-02-27 21:51:21 -0500

    apply clang-format

--- recursion/is_palindrome.cpp
+++ recursion/is_palindrome.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <string_view>
 
-constexpr bool is_palindrome(std::string_view sv)
-{
-    return sv.size() < 2             ? true
-           : sv.front() == sv.back() ? is_palindrome(sv.substr(1, sv.size() - 2))
-                                     : false;
+constexpr bool is_palindrome(std::string_view sv) {
+  return sv.size() < 2             ? true
+         : sv.front() == sv.back() ? is_palindrome(sv.substr(1, sv.size() - 2))
+                                   : false;
 }
 
-int main()
-{
-    std::cout << std::boolalpha << is_palindrome("kayak") << std::endl;
+int main() {
+  std::cout << std::boolalpha << is_palindrome("kayak") << std::endl;
 }
--- recursion/reverse_str.cpp
+++ recursion/reverse_str.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include <string>
 
-std::string reverse_str(std::string s)
-{
-    return s.empty() ? std::string{} : reverse_str(s.substr(1)) + s[0];
+std::string reverse_str(std::string s) {
+  return s.empty() ? std::string{} : reverse_str(s.substr(1)) + s[0];
 }
 
-int main()
-{
-    std::cout << reverse_str("perception") << std::endl;
-}
+int main() { std::cout << reverse_str("perception") << std::endl; }
