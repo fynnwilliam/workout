0768c9464cfad894e5c6ba328589a68266520fd4
fynnwilliam <fynnwilliam@gmail.com>
2025-04-22 22:41:17 +0100

    apply .clang-format to is_valid_email

--- is_valid_email_address.cpp
+++ is_valid_email_address.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <regex>
 
-bool is_valid_email(std::string_view item)
-{
-    static const std::regex pattern{"(\\w+)(\\.|_)?(\\w+)\\@(\\w+)(\\.(\\w+))+"};
-    return std::regex_match(item.data(), pattern);
+bool is_valid_email(std::string_view item) {
+  static const std::regex pattern{"(\\w+)(\\.|_)?(\\w+)\\@(\\w+)(\\.(\\w+))+"};
+  return std::regex_match(item.data(), pattern);
 }
 
-int main()
-{
-    std::cout << std::boolalpha << is_valid_email("example@email.com") << std::endl;
+int main() {
+  std::cout << std::boolalpha << is_valid_email("example@email.com")
+            << std::endl;
 }
