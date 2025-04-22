90d1bc76db449f9b52c191abd52bbd4bc7cc871b
fynnwilliam <fynnwilliam@gmail.com>
2025-04-22 23:18:57 +0100

    add benchmarking to is_valid_email_address
    For a char[30] on an i5-8350U CPU @ 1.70GHz, we got 2.68005 us

--- is_valid_email_address.cpp
+++ is_valid_email_address.cpp
@@ -1,3 +1,4 @@
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <regex>
 
@@ -10,7 +11,11 @@ TEST_CASE("is_valid_email") {
   REQUIRE(is_valid_email("example@email.com") == true);
   REQUIRE(is_valid_email("firstname.lastname@domain.com") == true);
   REQUIRE(is_valid_email("firstname_lastname@domain.com") == true);
-
   REQUIRE_FALSE(is_valid_email("example_email.com"));
   REQUIRE_FALSE(is_valid_email("example@email"));
+}
+
+TEST_CASE("is_valid_email", "[!benchmark]") {
+  std::string_view sv = "firstname.lastname@domain.com";
+  BENCHMARK("is_valid_email(sv)") { return is_valid_email(sv); };
 }
