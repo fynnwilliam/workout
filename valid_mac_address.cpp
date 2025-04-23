096a2736b6bebab2b086258f0275a1bca438d73a
fynnwilliam <fynnwilliam@gmail.com>
2025-04-23 12:32:34 +0100

    style: apply .clang-format

--- valid_mac_address.cpp
+++ valid_mac_address.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <regex>
 
-bool is_mac_address(std::string_view item)
-{
-    static const std::regex pattern{"((([a-fA-F]|\\d){2}\\:){5}([a-fA-F]|\\d){2})"};
-    return std::regex_match(item.data(), pattern);
+bool is_mac_address(std::string_view item) {
+  static const std::regex pattern{"((([a-fA-F]|\\d){2}\\:){5}([a-fA-F]|\\d){2})"};
+  return std::regex_match(item.data(), pattern);
 }
 
-int main()
-{
-    std::cout << std::boolalpha << is_mac_address("2a:27:c7:b7:3d:17") << '\n';
+int main() {
+  std::cout << std::boolalpha << is_mac_address("2a:27:c7:b7:3d:17") << '\n';
 }
