2f95fa9713487a961de4c0786e2894b84793f890
fynnwilliam <fynnwilliam@gmail.com>
2025-04-23 13:03:05 +0100

    test: add benchmarking
    3.32112 us on an i5-8350U CPU @ 1.70GHz with a valid mac

--- is_mac_address.cpp
+++ is_mac_address.cpp
@@ -1,3 +1,4 @@
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <regex>
 
@@ -14,4 +15,8 @@ TEST_CASE("is_mac_address") {
   REQUIRE(is_mac_address("00-1A-2B-3C-4D-5E") == true);
   REQUIRE(is_mac_address("2a:27:c7:z7:3d:17") == false);
   REQUIRE(is_mac_address("2a:27a:c7:b7:3d:17") == false);
+}
+
+TEST_CASE("is_mac_address", "[!benchmark]") {
+  BENCHMARK("") { return is_mac_address("01-23-45-67-89-ab"); };
 }
