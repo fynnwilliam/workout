08a3dc065053c90a194a945ae3dbd4ab2040a71e
fynnwilliam <fynnwilliam@gmail.com>
2025-04-24 15:12:46 +0100

    refactor: rename test case

--- atoi.cpp
+++ atoi.cpp
@@ -21,7 +21,7 @@ int _atoi(std::string_view s) {
   return number * sign;
 }
 
-TEST_CASE("_atoi(-1353)") {
+TEST_CASE("_atoi") {
   REQUIRE(_atoi("-1353") == -1353);
   REQUIRE(_atoi("1123456789") == 1'123'456'789);
 }
@@ -30,5 +30,5 @@ TEST_CASE("_atoi", "[!benchmark]") {
   REQUIRE(_atoi("-1353") == -1353);
   REQUIRE(_atoi("1123456789") == 1'123'456'789);
 
-  BENCHMARK("atoi(1'123'456'789)") { return _atoi("1123456789"); };
+  BENCHMARK("_atoi(1'123'456'789)") { return _atoi("1123456789"); };
 }
