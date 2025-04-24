60c8c91c5125462325fb885d9c2e5e59b5c8cb1f
fynnwilliam <fynnwilliam@gmail.com>
2025-04-24 13:20:12 +0100

    style: apply .clang-format

--- atoi.cpp
+++ atoi.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 
-int _atoi(std::string const& s)
-{
-    int index{};
-    int sign{1};
-    
-    if (s[index] == '-')
-    {
-        sign = -1;
-        ++index;
-    }
-    
-    int res{};
-    for ( ; s[index] != '\0'; ++index)
-    {
-        if (s[index] < '0' || s[index] > '9') break;
+int _atoi(std::string const& s) {
+  int index{};
+  int sign{1};
 
-        res = res * 10 + s[index] - '0';
-    }
-    
-    return res * sign;
+  if (s[index] == '-') {
+    sign = -1;
+    ++index;
+  }
+
+  int res{};
+  for (; s[index] != '\0'; ++index) {
+    if (s[index] < '0' || s[index] > '9')
+      break;
+
+    res = res * 10 + s[index] - '0';
+  }
+
+  return res * sign;
 }
 
-int main()
-{
-    std::cout << _atoi("-135d3") << '\n';
-    std::cout << std::atoi("-135d3") << std::endl;
+int main() {
+  std::cout << _atoi("-135d3") << '\n';
+  std::cout << std::atoi("-135d3") << std::endl;
 }
