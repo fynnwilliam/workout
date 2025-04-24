3a46bb5173c07b462b30ff0f03afc4ebe4709933
fynnwilliam <fynnwilliam@gmail.com>
2025-04-24 14:50:46 +0100

    refactor: merge previous and current impls
    This is a bit shorter and delivers about same performance

--- atoi.cpp
+++ atoi.cpp
@@ -1,27 +1,23 @@
-#include <array>
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 
-int _atoi(const char* b, const char* e) {
-  constexpr std::array<unsigned long, 10> pow10{
-      1'000'000'000UL, 100'000'000UL, 10'000'000UL, 1'000'000UL, 100'000UL,
-      10'000UL,        1'000UL,       100UL,        10UL,        1UL
-  };
+int _atoi(std::string_view s) {
+  auto b = s.begin();
+  auto e = s.end();
 
-  int sign = 1;
+  int sign{1};
   if (*b == '-') {
     sign = -1;
     ++b;
   }
 
-  assert(b < e);
-  int number = 0;
-  auto i = sizeof(pow10) / sizeof(pow10[0]) - (e - b);
-  for (; b != e; ++b) {
-    const auto digit = unsigned(*b) - '0';
+  int number{};
+  for (; b < e ; ++b) {
+    auto digit = unsigned(*b) - '0';
     assert(digit < 10);
-    number += pow10[i++] * digit;
+    number = number * 10 + digit;
   }
+
   return number * sign;
 }
 
