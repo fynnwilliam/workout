91270b3bc41e9802754c7d7f468001b2ba174cc0
fynnwilliam <fynnwilliam@gmail.com>
2025-04-25 18:00:07 +0100

    refactor: move swap in a local namespace
    for preventing calls to std::swap instead

--- swap_without_temp.cpp
+++ swap_without_temp.cpp
@@ -2,6 +2,7 @@
 #include <concepts>
 #include <string>
 
+namespace swt {
 template <typename T>
   requires std::integral<T> || std::floating_point<T>
 void swap(T& a, T& b) {
@@ -17,17 +18,18 @@ void swap(T& a, T& b) {
   b = a.substr(0, a.size() - b.size());
   a = a.substr(b.size());
 }
+} // namespace swt
 
 int main() {
   char a{'a'};
   char b{'b'};
 
-  ::swap(a, b);
+  swt::swap(a, b);
   assert(a == 'b' && b == 'a');
 
   std::string s1{"world"};
   std::string s2{"hello"};
 
-  ::swap(s1, s2);
+  swt::swap(s1, s2);
   assert(s1 == "hello" && s2 == "world");
 }
