444a8390fe3d0717f808cb7c3452018d91636a03
fynnwilliam <fynnwilliam@gmail.com>
2025-04-25 17:54:33 +0100

    refactor: remove func swap_without_temp
    we don't need the dispatch layer; concepts handles it

--- swap_without_temp.cpp
+++ swap_without_temp.cpp
@@ -18,23 +18,16 @@ void swap(T& a, T& b) {
   a = a.substr(b.size());
 }
 
-template <typename T> void swap_without_temp(T& a, T& b) {
-  if constexpr (std::same_as<T, std::string>)
-    return swap(a, b);
-  else
-    swap(a, b);
-}
-
 int main() {
   char a{'a'};
   char b{'b'};
 
-  swap_without_temp(a, b);
+  ::swap(a, b);
   assert(a == 'b' && b == 'a');
 
   std::string s1{"world"};
   std::string s2{"hello"};
 
-  swap_without_temp(s1, s2);
+  ::swap(s1, s2);
   assert(s1 == "hello" && s2 == "world");
 }
