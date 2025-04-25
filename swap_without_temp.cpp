8836d2a0d7fc8c3083339ef7b60a5c981973af44
fynnwilliam <fynnwilliam@gmail.com>
2025-04-25 17:26:05 +0100

    style: apply .clang-format

--- swap_without_temp.cpp
+++ swap_without_temp.cpp
@@ -3,41 +3,36 @@
 #include <string>
 
 template <typename T>
-    requires std::integral<T> || std::floating_point<T>
-void swap_n(T& a, T& b)
-{
-    a += b;
-    b = a - b;
-    a -= b;
+  requires std::integral<T> || std::floating_point<T>
+void swap_n(T& a, T& b) {
+  a += b;
+  b = a - b;
+  a -= b;
 }
 
-void swap_s(std::string& a, std::string& b)
-{
-    a.append(b);
-    b = a.substr(0, a.size() - b.size());
-    a = a.substr(b.size());
+void swap_s(std::string& a, std::string& b) {
+  a.append(b);
+  b = a.substr(0, a.size() - b.size());
+  a = a.substr(b.size());
 }
 
-template <typename T>
-void swap_without_temp(T& a, T& b)
-{
-    if constexpr (std::same_as<T, std::string>)
-        return swap_s(a, b);
-    else
-        swap_n(a, b);
+template <typename T> void swap_without_temp(T& a, T& b) {
+  if constexpr (std::same_as<T, std::string>)
+    return swap_s(a, b);
+  else
+    swap_n(a, b);
 }
 
-int main()
-{
-    char a{'a'};
-    char b{'b'};
-    
-    swap_without_temp(a, b);
-    assert(a == 'b' && b == 'a');
-    
-    std::string s1{"world"};
-    std::string s2{"hello"};
-    
-    swap_without_temp(s1, s2);
-    assert(s1 == "hello" && s2 == "world");
+int main() {
+  char a{'a'};
+  char b{'b'};
+
+  swap_without_temp(a, b);
+  assert(a == 'b' && b == 'a');
+
+  std::string s1{"world"};
+  std::string s2{"hello"};
+
+  swap_without_temp(s1, s2);
+  assert(s1 == "hello" && s2 == "world");
 }
