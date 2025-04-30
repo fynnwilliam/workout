ba8affa66f0d8d387866faeeacb64428571cbe47
fynnwilliam <fynnwilliam@gmail.com>
2025-04-30 11:36:09 +0100

    mark is_valid_email_address as nodiscard

--- is_valid_email_address.cpp
+++ is_valid_email_address.cpp
@@ -2,7 +2,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <regex>
 
-bool is_valid_email(std::string_view item) {
+[[nodiscard]] bool is_valid_email(std::string_view item) {
   static const std::regex pattern{"(\\w+)(\\.|_)?(\\w+)\\@(\\w+)(\\.(\\w+))+"};
   return std::regex_match(item.data(), pattern);
 }
