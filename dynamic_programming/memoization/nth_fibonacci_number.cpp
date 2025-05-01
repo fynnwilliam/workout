20c1d6ca2f1b392746f2d4b8e835f43552211dac
fynnwilliam <fynnwilliam@gmail.com>
2025-05-01 13:14:22 +0100

    style: apply .clang-format

--- dynamic_programming/memoization/nth_fibonacci_number.cpp
+++ dynamic_programming/memoization/nth_fibonacci_number.cpp
@@ -2,19 +2,17 @@
 #include <iostream>
 #include <unordered_map>
 
-unsigned long nth_fibonacci(int n)
-{
-    static std::unordered_map<int, unsigned long> m;
-    
-    return m.count(n) ? m[n] :
-           n <  1     ? 0    :
-           n == 1     ? 1    :
-           m[n] = nth_fibonacci(n - 1) + nth_fibonacci(n - 2);
+unsigned long nth_fibonacci(int n) {
+  static std::unordered_map<int, unsigned long> m;
+
+  return m.count(n) ? m[n]
+         : n < 1    ? 0
+         : n == 1   ? 1
+                    : m[n] = nth_fibonacci(n - 1) + nth_fibonacci(n - 2);
 }
 
-int main()
-{
-    std::cout << nth_fibonacci(93) << std::endl;
-    
-    assert(nth_fibonacci(93) == 12'200'160'415'121'876'738zu);
+int main() {
+  std::cout << nth_fibonacci(93) << std::endl;
+
+  assert(nth_fibonacci(93) == 12'200'160'415'121'876'738zu);
 }
