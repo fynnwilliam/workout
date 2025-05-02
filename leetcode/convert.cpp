907ea07b021ff81ee6b6fa76bf0bbf8a54174fd5
fynnwilliam <fynnwilliam@gmail.com>
2025-05-02 12:27:23 +0100

    test: add benchmark for zigzag conversion

--- leetcode/convert.cpp
+++ leetcode/convert.cpp
@@ -1,7 +1,19 @@
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("convert") {
   REQUIRE(convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR");
   REQUIRE(convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI");
   REQUIRE(convert("PAYPALISHIRING", 5) == "PHASIYIRPLIGAN");
+}
+
+TEST_CASE("convert", "[!benchmark]") {
+  REQUIRE(convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR");
+  REQUIRE(convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI");
+  REQUIRE(convert("PAYPALISHIRING", 5) == "PHASIYIRPLIGAN");
+
+  std::string s(1'000'000, 'a');
+  BENCHMARK("convert(std::string s(1'000'000, 'a'), 10)") {
+    return convert(s, 10);
+  };
 }
