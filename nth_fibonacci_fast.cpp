e397f8aaa61f6b5f97fd87e7d5f60a495275457e
fynnwilliam <fynnwilliam@gmail.com>
2025-05-08 12:39:32 +0100

    test: add benchmark
    0.42 ns - 0.92 ns on a Intel(R) Core(TM) i5-8350U CPU @ 1.70GHz

--- nth_fibonacci_fast.cpp
+++ nth_fibonacci_fast.cpp
@@ -1,3 +1,4 @@
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 const auto fib_fast = [](uint8_t n) {
@@ -21,4 +22,8 @@ TEST_CASE("fib_fast") {
   REQUIRE(fib_fast(50) == 12'586'269'025zu);
   REQUIRE(fib_fast(71) == 308'061'521'170'129zu);
   REQUIRE(fib_fast(93) == 12'200'160'415'121'876'738zu);
+}
+
+TEST_CASE("fib_fast", "[!benchmark]") {
+  BENCHMARK("fib_fast 71") { return fib_fast(93); };
 }
