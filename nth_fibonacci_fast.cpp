7b695d5a7125a24811d1dfdb4d36bb42af434923
fynnwilliam <fynnwilliam@gmail.com>
2025-05-08 12:33:57 +0100

    test: add test case

--- nth_fibonacci_fast.cpp
+++ nth_fibonacci_fast.cpp
@@ -1,6 +1,4 @@
-#include <cassert>
-#include <cstddef>
-#include <cstdint>
+#include <catch2/catch_test_macros.hpp>
 
 const auto fib_fast = [](uint8_t n) {
   assert(n < 94);
@@ -16,3 +14,11 @@ const auto fib_fast = [](uint8_t n) {
 
   return result;
 };
+
+TEST_CASE("fib_fast") {
+  REQUIRE(fib_fast(2) == 1zu);
+  REQUIRE(fib_fast(39) == 63'245'986zu);
+  REQUIRE(fib_fast(50) == 12'586'269'025zu);
+  REQUIRE(fib_fast(71) == 308'061'521'170'129zu);
+  REQUIRE(fib_fast(93) == 12'200'160'415'121'876'738zu);
+}
