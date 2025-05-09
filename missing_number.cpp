9ed167bb73d45b3b10baaa36820fa6c45cea22f4
fynnwilliam <fynnwilliam@gmail.com>
2025-05-09 23:18:04 +0100

    style: apply .clang-format

--- missing_number.cpp
+++ missing_number.cpp
@@ -1,29 +1,26 @@
-#include <iostream>
-#include <numeric>
-#include <ctime>
 #include <array>
 #include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <numeric>
 
-unsigned sum_of_n(std::size_t n)
-{
-    return n * (n + 1) / 2;
+unsigned sum_of_n(std::size_t n) {
+  return n * (n + 1) / 2;
 }
 
-int missing_number(auto const& numbers)
-{
-    unsigned expected_sum = sum_of_n(numbers.size());
-    unsigned current_sum  = std::accumulate(numbers.begin(), numbers.end(), 0);
-    
-    return expected_sum - current_sum;
+int missing_number(auto const& numbers) {
+  unsigned expected_sum = sum_of_n(numbers.size());
+  unsigned current_sum = std::accumulate(numbers.begin(), numbers.end(), 0);
+
+  return expected_sum - current_sum;
 }
 
-int main()
-{
-    std::array<int, 100> numbers;
-    std::iota(numbers.begin(), numbers.end(), 1);
-    
-    std::srand(std::time(nullptr));
-    numbers[std::rand() % 100] = 0;
-    
-    std::cout << missing_number(numbers) << std::endl;
+int main() {
+  std::array<int, 100> numbers;
+  std::iota(numbers.begin(), numbers.end(), 1);
+
+  std::srand(std::time(nullptr));
+  numbers[std::rand() % 100] = 0;
+
+  std::cout << missing_number(numbers) << std::endl;
 }
