a96174d4d7d189db567e19c9f211b972b492e219
fynnwilliam <fynnwilliam@gmail.com>
2025-05-12 01:42:26 +0100

    refactor: remove duplicate header file

--- nth_largest_element.cpp
+++ nth_largest_element.cpp
@@ -2,7 +2,6 @@
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <set>
-#include <vector>
 
 std::set<int> extract_unique(std::vector<int> const& v) {
   std::set<int> s;
