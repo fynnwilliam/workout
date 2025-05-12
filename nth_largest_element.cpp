2d0e449c201b0cea185e5e7add0d55a2bd7a081c
fynnwilliam <fynnwilliam@gmail.com>
2025-05-12 01:25:54 +0100

    test: add benchmarking
    completes in 238.661 ms on a i5-8350U CPU @ 1.70GHz

--- nth_largest_element.cpp
+++ nth_largest_element.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <set>
 #include <vector>
@@ -26,4 +28,13 @@ int nth_largest_element(auto const& v, int index) {
 TEST_CASE("nth_largest_element") {
   std::vector<int> v{1, 3, 2, 17, 18, 8, 19, 22, 2, 10, 11, 11, 133};
   REQUIRE(nth_largest_element(v, 9) == 19);
+}
+
+TEST_CASE("nth_largest_element", "[!benchmark]") {
+  std::vector<int> numbers(1'000'000);
+  std::ranges::iota(numbers, 1);
+
+  BENCHMARK("nth_largest_element(numbers)") {
+    return nth_largest_element(numbers, 1'000);
+  };
 }
