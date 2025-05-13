69821bef50ec3eef5e20ef2b6d9d8884f0911bbe
fynnwilliam <fynnwilliam@gmail.com>
2025-05-13 19:31:00 +0100

    style: apply .clang-format

--- shortest_unique_substring.cpp
+++ shortest_unique_substring.cpp
@@ -2,32 +2,29 @@
 #include <iostream>
 #include <string>
 
-std::string check_sub(std::string const& s, auto& arr)
-{
-    for(auto const& x : arr)
-    {
-        if (s.find(x) == std::string::npos)
-            return std::string{};
-    }
-    
-    return s;
+std::string check_sub(std::string const& s, auto& arr) {
+  for (auto const& x : arr) {
+    if (s.find(x) == std::string::npos)
+      return std::string{};
+  }
+
+  return s;
 }
 
-std::string smallest_substr(std::string const& s, auto const& arr)
-{    
-    if (arr.size() > s.size()) return std::string{};
-    
-    for (std::size_t i{}; i <= s.size() - arr.size(); ++i)
-    {
-        std::string sub_str = check_sub(s.substr(i, arr.size()), arr);
-        if (!sub_str.empty()) return sub_str;
-    }
-    
+std::string smallest_substr(std::string const& s, auto const& arr) {
+  if (arr.size() > s.size())
     return std::string{};
+
+  for (std::size_t i{}; i <= s.size() - arr.size(); ++i) {
+    std::string sub_str = check_sub(s.substr(i, arr.size()), arr);
+    if (!sub_str.empty())
+      return sub_str;
+  }
+
+  return std::string{};
 }
 
-int main()
-{
-    std::vector v{'o', 't', 'a'};
-    std::cout << smallest_substr("hippopotamus", v) << std::endl;
+int main() {
+  std::vector v{'o', 't', 'a'};
+  std::cout << smallest_substr("hippopotamus", v) << std::endl;
 }
