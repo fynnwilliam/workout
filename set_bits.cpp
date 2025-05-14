8555e94f923a0364ba977afb20f0a2832e9f43ed
fynnwilliam <fynnwilliam@gmail.com>
2025-05-14 11:33:34 +0100

    style: apply .clang-format

--- set_bits.cpp
+++ set_bits.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
 
-constexpr int set_bits(int n) // Brian Kernighan's approach
-{
-    int count{};
-    for ( ; n ; count++)
-        n &= n - 1;
-        
-    return count;
+constexpr int set_bits(int n) { // Brian Kernighan's approach
+  int count{};
+  for (; n; count++)
+    n &= n - 1;
+
+  return count;
 }
 
-constexpr int _set_bits(int n) // other approach
-{
-    int count{};
-    
-    while(n)
-    {
-        count += n & 1;
-        n >>= 1;
-    }
-    
-    return count;
+constexpr int _set_bits(int n) { // other approach
+  int count{};
+
+  while (n) {
+    count += n & 1;
+    n >>= 1;
+  }
+
+  return count;
 }
 
-int main()
-{
-    std::cout <<  set_bits(128) << '\n'
-              << _set_bits(128) << std::endl;
+int main() {
+  std::cout << set_bits(128) << '\n' << _set_bits(128) << std::endl;
 }
