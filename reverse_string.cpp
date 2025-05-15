3c5c197be19d9e949aa92b00cf19032bc5ec3b63
fynnwilliam <fynnwilliam@gmail.com>
2025-05-15 14:21:45 +0100

    test: add benchmarking
    current impl finishes in 644.53 us on a i5-8350U CPU @ 1.70GHz

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -1,3 +1,4 @@
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <string>
 
@@ -17,4 +18,11 @@ std::string reverse_str(std::string s) {
 TEST_CASE("reverse_str") {
   using namespace std::string_literals;
   REQUIRE(reverse_str("how are you doing?") == "?gniod uoy era woh"s);
+}
+
+TEST_CASE("reverse_str", "[!benchmark]") {
+  std::string s(1'000'000, ' ');
+  BENCHMARK("reverse_str(s)") {
+    return reverse_str(s);
+  };
 }
