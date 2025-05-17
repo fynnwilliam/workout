a4eb3b9552e945eb0c5f0264242450b80709ad4e
fynnwilliam <fynnwilliam@gmail.com>
2025-05-17 22:19:02 +0100

    style: apply .clang-format

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,30 +1,29 @@
 #include <algorithm>
-#include <iterator>
 #include <cassert>
+#include <iterator>
 #include <vector>
 
 template <typename T>
-void _insertion_sort(T& container, typename T::value_type value)
-{
-    auto itr{std::ranges::upper_bound(container, value)};
-    container.insert(itr, value);
+void _insertion_sort(T& container, typename T::value_type value) {
+  auto itr{std::ranges::upper_bound(container, value)};
+  container.insert(itr, value);
 }
 
 template <typename T, typename... U>
-    requires requires(T x, typename T::value_type value)
-        {{ x.begin() } -> std::forward_iterator; x.insert(x.begin(), value);}
-    && (std::same_as<typename T::value_type, U> && ...)
-void insertion_sort(T& container, U... args)
-{
-    if (std::initializer_list<typename T::value_type> values{args...}; values.size())
-        for (auto const& value : values)
-            _insertion_sort(container, value);
+  requires requires(T x, typename T::value_type value) {
+    { x.begin() } -> std::forward_iterator;
+    x.insert(x.begin(), value);
+  } && (std::same_as<typename T::value_type, U> && ...)
+void insertion_sort(T& container, U... args) {
+  if (std::initializer_list<typename T::value_type> values{args...};
+      values.size())
+    for (auto const& value : values)
+      _insertion_sort(container, value);
 }
 
-int main()
-{
-    std::vector<int>  numbers{1, 3, 4, 6, 7, 8};
+int main() {
+  std::vector<int> numbers{1, 3, 4, 6, 7, 8};
 
-    insertion_sort(numbers, 2, 0, 9, 5);
-    assert((numbers == std::vector{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+  insertion_sort(numbers, 2, 0, 9, 5);
+  assert((numbers == std::vector{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
 }
