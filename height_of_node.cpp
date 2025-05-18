37380a210cdde610eb0ffcbeff55e0d6048d74e4
fynnwilliam <fynnwilliam@gmail.com>
2025-05-18 22:39:23 +0100

    refactor: take parameters by ref& to reduce possible calls with nullptr

--- height_of_node.cpp
+++ height_of_node.cpp
@@ -8,34 +8,34 @@ struct node {
   unsigned height = 0;
 };
 
-constexpr void compute_height(node*);
+constexpr void compute_height(node&);
 
-constexpr void leaf_height(node* n) {
-  n->height = 0;
+constexpr void leaf_height(node& n) {
+  n.height = 0;
 }
 
-constexpr void left_height(node* n) {
-  auto& [left, _, height] = *n;
-  compute_height(left);
+constexpr void left_height(node& n) {
+  auto& [left, _, height] = n;
+  compute_height(*left);
   height = left->height + 1;
 }
 
-constexpr void right_height(node* n) {
-  auto& [_, right, height] = *n;
-  compute_height(right);
+constexpr void right_height(node& n) {
+  auto& [_, right, height] = n;
+  compute_height(*right);
   height = right->height + 1;
 }
 
-constexpr void mountain_height(node* n) {
-  auto& [left, right, height] = *n;
-  compute_height(left);
-  compute_height(right);
+constexpr void mountain_height(node& n) {
+  auto& [left, right, height] = n;
+  compute_height(*left);
+  compute_height(*right);
 
   height = std::max(left->height, right->height) + 1;
 }
 
-constexpr void compute_height(node* n) {
-  auto& [left, right, _] = *n;
+constexpr void compute_height(node& n) {
+  auto& [left, right, _] = n;
   !left && !right  ? leaf_height(n)
   : left && right  ? mountain_height(n)
   : left && !right ? left_height(n)
@@ -44,7 +44,7 @@ constexpr void compute_height(node* n) {
 
 TEST_CASE("height of a default node should be zero(0)") {
   node n;
-  compute_height(&n);
+  compute_height(n);
   REQUIRE(n.height == 0u);
 }
 
@@ -63,12 +63,12 @@ node p{&o, &z};
 } // namespace
 
 TEST_CASE("function calls in fib(7) has a height of 5 when shown as a tree") {
-  compute_height(&p);
+  compute_height(p);
   REQUIRE(p.height == 5u);
 }
 
 TEST_CASE("compute_height", "[!benchmark]") {
   BENCHMARK("compute_height(&p)") {
-    return compute_height(&p);
+    return compute_height(p);
   };
 }
