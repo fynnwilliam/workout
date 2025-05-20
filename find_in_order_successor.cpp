e35d9caa358c01f3bb424c27fce20f914f4d1cd7
fynnwilliam <fynnwilliam@gmail.com>
2025-05-21 00:11:31 +0100

    refactor: mark methods constexpr to allow compile time calls

--- find_in_order_successor.cpp
+++ find_in_order_successor.cpp
@@ -9,25 +9,25 @@ constexpr bool operator>(const node& lhs, const node& rhs) noexcept {
   return lhs.value > rhs.value;
 }
 
-node* leftmost_key(node& n) {
+constexpr node* leftmost_key(node& n) {
   return n.left ? leftmost_key(*n.left) : &n;
 }
 
-node* root_node(node& n) {
+constexpr node* root_node(node& n) {
   return n.parent ? root_node(*n.parent) : &n;
 }
 
-bool is_left_alligned(node& n) {
+constexpr bool is_left_alligned(node& n) {
   auto& [parent, _, _, _] = n;
   return parent && &n == parent->left;
 }
 
-node* climb(node& n) {
+constexpr node* climb(node& n) {
   node* root = root_node(n);
   return *root > n ? root : nullptr;
 }
 
-node* in_order_successor(node& n) {
+constexpr node* in_order_successor(node& n) {
   auto& [parent, _, right, _] = n;
   if (right != nullptr) {
     return leftmost_key(*right);
