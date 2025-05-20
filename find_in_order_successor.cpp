5089c8b406dbb492e75f4f3e39bc073626ce5d6b
fynnwilliam <fynnwilliam@gmail.com>
2025-05-21 00:16:25 +0100

    refactor: mark methods as nodiscard
    it won't be a good idea ignoring returned values from these

--- find_in_order_successor.cpp
+++ find_in_order_successor.cpp
@@ -5,29 +5,30 @@ struct node {
   int value = 0;
 };
 
-constexpr bool operator>(const node& lhs, const node& rhs) noexcept {
+[[nodiscard]] constexpr bool
+operator>(const node& lhs, const node& rhs) noexcept {
   return lhs.value > rhs.value;
 }
 
-constexpr node* leftmost_key(node& n) {
+[[nodiscard]] constexpr node* leftmost_key(node& n) {
   return n.left ? leftmost_key(*n.left) : &n;
 }
 
-constexpr node* root_node(node& n) {
+[[nodiscard]] constexpr node* root_node(node& n) {
   return n.parent ? root_node(*n.parent) : &n;
 }
 
-constexpr bool is_left_alligned(node& n) {
+[[nodiscard]] constexpr bool is_left_alligned(node& n) {
   auto& [parent, _, _, _] = n;
   return parent && &n == parent->left;
 }
 
-constexpr node* climb(node& n) {
+[[nodiscard]] constexpr node* climb(node& n) {
   node* root = root_node(n);
   return *root > n ? root : nullptr;
 }
 
-constexpr node* in_order_successor(node& n) {
+[[nodiscard]] constexpr node* in_order_successor(node& n) {
   auto& [parent, _, right, _] = n;
   if (right != nullptr) {
     return leftmost_key(*right);
