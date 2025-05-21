740caa67b90c5669ed8c69e8dae3b3ad15eb861a
fynnwilliam <fynnwilliam@gmail.com>
2025-05-21 02:09:12 +0100

    refactor: apply const to communicate immutability

--- find_in_order_successor.cpp
+++ find_in_order_successor.cpp
@@ -12,25 +12,25 @@ struct node {
   return lhs.value > rhs.value;
 }
 
-[[nodiscard]] constexpr node* leftmost_key(node& n) {
+[[nodiscard]] constexpr const node* leftmost_key(const node& n) {
   return n.left ? leftmost_key(*n.left) : &n;
 }
 
-[[nodiscard]] constexpr node* root_node(node& n) {
+[[nodiscard]] constexpr const node* root_node(const node& n) {
   return n.parent ? root_node(*n.parent) : &n;
 }
 
-[[nodiscard]] constexpr bool is_left_alligned(node& n) {
+[[nodiscard]] constexpr bool is_left_alligned(const node& n) {
   auto& [parent, _, _, _] = n;
   return parent && &n == parent->left;
 }
 
-[[nodiscard]] constexpr node* climb(node& n) {
-  node* root = root_node(n);
+[[nodiscard]] constexpr const node* climb(const node& n) {
+  const node* root = root_node(n);
   return *root > n ? root : nullptr;
 }
 
-[[nodiscard]] constexpr node* in_order_successor(node& n) {
+[[nodiscard]] constexpr const node* in_order_successor(const node& n) {
   auto& [parent, _, right, _] = n;
   if (right != nullptr) {
     return leftmost_key(*right);
