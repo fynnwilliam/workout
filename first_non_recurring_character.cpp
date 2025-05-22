e024d25dd0a0474cfecb2c7be43adaafed3bf99a
fynnwilliam <fynnwilliam@gmail.com>
2025-05-22 22:56:06 +0100

    style: apply .clang-format

--- first_non_recurring_character.cpp
+++ first_non_recurring_character.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
 #include <string>
 
-char first_non_recurring_char(std::string const& s)
-{
-    for (std::size_t i{}; i < s.size(); ++i)
-    {
-        if (s.find_last_of(s[i]) == i)
-            return s[i];
-    }
-    
-    return ' ';
+char first_non_recurring_char(std::string const& s) {
+  for (std::size_t i{}; i < s.size(); ++i) {
+    if (s.find_last_of(s[i]) == i)
+      return s[i];
+  }
+
+  return ' ';
 }
 
-std::string declare(char const& c)
-{
-    return c == ' ' ? "none found" : std::string{c};
+std::string declare(char const& c) {
+  return c == ' ' ? "none found" : std::string{c};
 }
 
-int main()
-{
-    char c =  first_non_recurring_char(std::string{"character"});
-    
-    std::cout << declare(c) << std::endl;
+int main() {
+  char c = first_non_recurring_char(std::string{"character"});
+
+  std::cout << declare(c) << std::endl;
 }
