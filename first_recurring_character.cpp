55f24e1442f78bed71bdd121bd6502365eb80dcf
fynnwilliam <fynnwilliam@gmail.com>
2025-05-24 13:08:52 +0100

    style: apply .clang-format

--- first_recurring_character.cpp
+++ first_recurring_character.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
 #include <string>
 
-char first_recurring_char(std::string const& s)
-{   
-    for (std::size_t i{}; i < s.size() - 1; ++i)
-    {
-        if (auto pos = s.find_last_of(s[i]); pos > i && pos < s.size())
-            return s[i];
-    }
-    
-    return ' ';
+char first_recurring_char(std::string const& s) {
+  for (std::size_t i{}; i < s.size() - 1; ++i) {
+    if (auto pos = s.find_last_of(s[i]); pos > i && pos < s.size())
+      return s[i];
+  }
+
+  return ' ';
 }
 
-std::string declare(char const& c)
-{
-    return c == ' ' ? "none found" : std::string{c};
+std::string declare(char const& c) {
+  return c == ' ' ? "none found" : std::string{c};
 }
 
-int main()
-{
-    char c = first_recurring_char(std::string{"character"});
-    
-    std::cout << declare(c) << std::endl;
+int main() {
+  char c = first_recurring_char(std::string{"character"});
+
+  std::cout << declare(c) << std::endl;
 }
