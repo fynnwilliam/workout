756344935f7e2a011c0d0b51da618e18c26c90e0
fynnwilliam <fynnwilliam@gmail.com>
2025-05-24 13:25:34 +0100

    test: add more assertions

--- first_recurring_character.cpp
+++ first_recurring_character.cpp
@@ -13,4 +13,7 @@ char first_recurring_char(std::string const& s) {
 TEST_CASE("first_recurring_char") {
   using namespace std::string_literals;
   REQUIRE(first_recurring_char("character"s) == 'c');
+  REQUIRE(first_recurring_char("recurring"s) == 'r');
+  REQUIRE(first_recurring_char("maximised"s) == 'm');
+  REQUIRE(first_recurring_char("applejack"s) == 'a');
 }
