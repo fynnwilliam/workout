673aad0935cfd7efaa9c1eec6195b4343441825c
fynnwilliam <fynnwilliam@gmail.com>
2025-05-25 14:07:08 +0100

    style: apply .clang-format

--- closest_coin.cpp
+++ closest_coin.cpp
@@ -1,36 +1,37 @@
 #include "coin.h"
 
-void closest_coin(point a, std::vector<point> coins)
-{
-    for (auto const& c : coins)
-    {
-        point b = _closest_coin(a, coins);
+void closest_coin(point a, std::vector<point> coins) {
+  for (auto const& c : coins) {
+    point b = _closest_coin(a, coins);
 
-        std::erase_if(coins, [&](auto const& c) { return c == b; });
+    std::erase_if(coins, [&](auto const& c) { return c == b; });
 
-        std::cout << a << " -> " << b << '\n';
-        a.move_to(b);
-    }
-    
-    std::cout << '\n';
+    std::cout << a << " -> " << b << '\n';
+    a.move_to(b);
+  }
+
+  std::cout << '\n';
 }
 
-void shortest_path(point a, std::vector<point> coins)
-{
-    auto coins_{coin_list(coins)};
-    
-    for (auto const& coin : _shortest_path(a, coins_))
-    {
-        std::cout << a << " -> " << coin.location() << '\n';
-        a.move_to(coin.location());
-    }
+void shortest_path(point a, std::vector<point> coins) {
+  auto coins_{coin_list(coins)};
+
+  for (auto const& coin : _shortest_path(a, coins_)) {
+    std::cout << a << " -> " << coin.location() << '\n';
+    a.move_to(coin.location());
+  }
 }
 
-int main()
-{
-    point a{0, 1};
-    std::vector<point> coins{{1, 3}, {2, 0}, {0, 2}, {4, 4}, {0, 0}};
+int main() {
+  point a{0, 1};
+  std::vector<point> coins{
+      {1, 3},
+      {2, 0},
+      {0, 2},
+      {4, 4},
+      {0, 0}
+  };
 
-    closest_coin(a, coins);
-    shortest_path(a, coins);
+  closest_coin(a, coins);
+  shortest_path(a, coins);
 }
--- coin.h
+++ coin.h
@@ -1,121 +1,109 @@
 #pragma once
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 #include "point.h"
 
-class coin
-{
+class coin {
 private:
-    int path_{};
-    point location_;
-    std::vector<coin> neighbours_;
-    
+  int path_{};
+  point location_;
+  std::vector<coin> neighbours_;
+
 public:
-    coin() = delete;
-    coin(point p) : location_{p} {}
-    
-    int path() const { return path_; }
-    point const& location() const { return location_; }
-    std::vector<coin> const& neighbours() const { return neighbours_; }
-    
-    void update_neighbours(std::vector<coin> coins)
-    {
-        point current_location{location_};
-        
-        for (auto const& c : coins)
-		{
-		    coin b = closest_coin(current_location, coins);
-		    
-		    std::erase_if(coins, [&](auto const& c) { return c == b; });
+  coin() = delete;
+  coin(point p) : location_{p} {}
 
-		    neighbours_.push_back(b);
-		    current_location.move_to(b.location());
-		}
+  int path() const {
+    return path_;
+  }
+  point const& location() const {
+    return location_;
+  }
+  std::vector<coin> const& neighbours() const {
+    return neighbours_;
+  }
+
+  void update_neighbours(std::vector<coin> coins) {
+    point current_location{location_};
+
+    for (auto const& c : coins) {
+      coin b = closest_coin(current_location, coins);
+
+      std::erase_if(coins, [&](auto const& c) { return c == b; });
+
+      neighbours_.push_back(b);
+      current_location.move_to(b.location());
     }
-    
-    void update_path()
-    {
-        if (neighbours_.size() == 1)
-        {
-            path_ = 0;
-        }
-        else
-        {
-		    for (std::size_t i{}; i < neighbours_.size() - 1; ++i)
-		    {
-		        path_ += neighbours_[i].location() - neighbours_[i + 1].location();
-		    }
-		}
+  }
+
+  void update_path() {
+    if (neighbours_.size() == 1) {
+      path_ = 0;
+    } else {
+      for (std::size_t i{}; i < neighbours_.size() - 1; ++i) {
+        path_ += neighbours_[i].location() - neighbours_[i + 1].location();
+      }
     }
-    
-    friend coin closest_coin(point c, std::vector<coin> coins)
-	{
-		coin closest_c{coins[0]};
-		
-		for (auto const& coin : coins)
-		{
-		    if (c - coin.location() < c - closest_c.location())
-		        closest_c = coin;
-		}
+  }
 
-		return closest_c;
-	}
-	
-	inline bool operator==(coin const& rhs) const
-	{
-	    return path() == rhs.path()
-	        && location() == rhs.location()
-	        && compare(neighbours_, rhs.neighbours_);
-	}
-	
-	friend bool compare(auto const& a, auto const& b)
-	{
-	    return a == b;
-	}
-};
+  friend coin closest_coin(point c, std::vector<coin> coins) {
+    coin closest_c{coins[0]};
 
-point _closest_coin(point player, std::vector<point> coins)
-{
-    point closest_c{coins[0]};
-    
-    for (auto const& coin : coins)
-    {
-        if (player - coin < player - closest_c)
-            closest_c = coin;
+    for (auto const& coin : coins) {
+      if (c - coin.location() < c - closest_c.location())
+        closest_c = coin;
     }
 
     return closest_c;
+  }
+
+  inline bool operator==(coin const& rhs) const {
+    return path() == rhs.path() && location() == rhs.location() &&
+           compare(neighbours_, rhs.neighbours_);
+  }
+
+  friend bool compare(auto const& a, auto const& b) {
+    return a == b;
+  }
+};
+
+point _closest_coin(point player, std::vector<point> coins) {
+  point closest_c{coins[0]};
+
+  for (auto const& coin : coins) {
+    if (player - coin < player - closest_c)
+      closest_c = coin;
+  }
+
+  return closest_c;
 }
 
-std::vector<coin> _shortest_path(point player, std::vector<coin>& coins)
-{
-    if (coins.empty()) return coins;
-    
-    for (auto& coin : coins)
-    {
-        coin.update_neighbours(coins);
-        coin.update_path();
-    }
-    
-    auto min{coins[0]};
-    int min_base_length{player - min.location()};
-    for (auto const& coin : coins)
-    {
-        int base_length{player - coin.location()};
-        if (base_length + coin.path() < min_base_length + min.path())
-            min = coin;
-    }
-    
-    return min.neighbours();
+std::vector<coin> _shortest_path(point player, std::vector<coin>& coins) {
+  if (coins.empty())
+    return coins;
+
+  for (auto& coin : coins) {
+    coin.update_neighbours(coins);
+    coin.update_path();
+  }
+
+  auto min{coins[0]};
+  int min_base_length{player - min.location()};
+  for (auto const& coin : coins) {
+    int base_length{player - coin.location()};
+    if (base_length + coin.path() < min_base_length + min.path())
+      min = coin;
+  }
+
+  return min.neighbours();
 }
 
-std::vector<coin> coin_list(std::vector<point> coins)
-{
-    std::vector<coin> c;
-    
-    for (auto const& coin : coins)
-        c.push_back(coin);
-    
-    return c;
+std::vector<coin> coin_list(std::vector<point> coins) {
+  std::vector<coin> c;
+
+  for (auto const& coin : coins)
+    c.push_back(coin);
+
+  return c;
 }
