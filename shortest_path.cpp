4a4001f096b76856c027ba02518329d1beab6c85
fynnwilliam <fynnwilliam@gmail.com>
2025-05-26 23:18:06 +0100

    refactor: rename _shortest_path -> shortest_path

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -1,7 +1,7 @@
 #include "coin.h"
 #include <catch2/catch_test_macros.hpp>
 
-std::vector<coin> _shortest_path(point player, std::vector<coin>& coins) {
+std::vector<coin> shortest_path(point player, std::vector<coin>& coins) {
   if (coins.empty())
     return coins;
 
@@ -21,7 +21,7 @@ std::vector<coin> _shortest_path(point player, std::vector<coin>& coins) {
   return min.neighbours();
 }
 
-TEST_CASE("_shortest_path") {
+TEST_CASE("shortest_path") {
   point a{0, 1};
   std::vector<coin> coins{
       point{1, 3},
@@ -32,7 +32,7 @@ TEST_CASE("_shortest_path") {
   };
 
   REQUIRE(
-      (_shortest_path(a, coins) ==
+      (shortest_path(a, coins) ==
        std::vector<coin>{
            point{0, 1},
            point{2, 0},
