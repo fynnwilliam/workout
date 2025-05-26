1da08a02bf22d9c95f2319c15fc5ee3294bf9bcc
fynnwilliam <fynnwilliam@gmail.com>
2025-05-26 23:13:53 +0100

    test: add a test case for shortest_path

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -1,4 +1,5 @@
 #include "coin.h"
+#include <catch2/catch_test_macros.hpp>
 
 std::vector<coin> _shortest_path(point player, std::vector<coin>& coins) {
   if (coins.empty())
@@ -18,4 +19,27 @@ std::vector<coin> _shortest_path(point player, std::vector<coin>& coins) {
   }
 
   return min.neighbours();
+}
+
+TEST_CASE("_shortest_path") {
+  point a{0, 1};
+  std::vector<coin> coins{
+      point{1, 3},
+      point{2, 0},
+      point{0, 2},
+      point{4, 4},
+      point{0, 0}
+  };
+
+  REQUIRE(
+      (_shortest_path(a, coins) ==
+       std::vector<coin>{
+           point{0, 1},
+           point{2, 0},
+           point{0, 0},
+           point{0, 2},
+           point{1, 3},
+           point{4, 4}
+  })
+  );
 }
