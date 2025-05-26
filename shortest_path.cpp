6b457643569fb140390a526e500a6ded684bcf02
fynnwilliam <fynnwilliam@gmail.com>
2025-05-26 23:39:26 +0100

    test: updated test implementation

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -1,7 +1,7 @@
 #include "coin.h"
 #include <catch2/catch_test_macros.hpp>
 
-std::vector<coin> shortest_path(point player, std::vector<coin>& coins) {
+std::vector<coin> shortest_path(point player, std::vector<coin> coins) {
   if (coins.empty())
     return coins;
 
@@ -22,24 +22,18 @@ std::vector<coin> shortest_path(point player, std::vector<coin>& coins) {
 }
 
 TEST_CASE("shortest_path") {
-  point a{0, 1};
-  std::vector<coin> coins{
-      point{1, 3},
-      point{2, 0},
-      point{0, 2},
-      point{4, 4},
-      point{0, 0}
-  };
-
-  REQUIRE(
-      (shortest_path(a, coins) ==
-       std::vector<coin>{
-           point{0, 1},
-           point{2, 0},
-           point{0, 0},
-           point{0, 2},
-           point{1, 3},
-           point{4, 4}
-  })
+  const auto coins = shortest_path(
+      point{
+          0, 1
+  },
+      std::vector<coin>{
+          point{1, 3}, point{2, 0}, point{0, 2}, point{4, 4}, point{0, 0}
+      }
   );
+
+  REQUIRE(coins[0].location() == point{2, 0});
+  REQUIRE(coins[1].location() == point{0, 0});
+  REQUIRE(coins[2].location() == point{0, 2});
+  REQUIRE(coins[3].location() == point{1, 3});
+  REQUIRE(coins[4].location() == point{4, 4});
 }
