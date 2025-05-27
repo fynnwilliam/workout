747b566ca64d89f40c2bd68dd3c9182ee2bc9e2f
fynnwilliam <fynnwilliam@gmail.com>
2025-05-27 20:40:23 +0100

    refactor: closest_coin should accept coins

--- closest_coin.cpp
+++ closest_coin.cpp
@@ -1,12 +1,12 @@
 #include "coin.h"
 #include <catch2/catch_test_macros.hpp>
 
-point closest_coin(point player, const std::vector<point>& coins) {
-  point closest_c{coins[0]};
+coin closest_coin(point player, const std::vector<coin>& coins) {
+  coin closest_c{coins[0]};
 
-  auto minimum_distance = player - closest_c;
+  auto minimum_distance = player - closest_c.location();
   for (auto const& coin : coins) {
-    if (auto distance = player - coin; distance < minimum_distance) {
+    if (auto distance = player - coin.location(); distance < minimum_distance) {
       closest_c = coin;
       minimum_distance = distance;
     }
@@ -16,13 +16,13 @@ point closest_coin(point player, const std::vector<point>& coins) {
 }
 
 TEST_CASE("closest_coin") {
-  std::vector<point> coins{
-      {1, 3},
-      {2, 0},
-      {0, 2},
-      {4, 4},
-      {0, 0}
+  std::vector<coin> coins{
+      point{1, 3},
+      point{2, 0},
+      point{0, 2},
+      point{4, 4},
+      point{0, 0}
   };
 
-  REQUIRE(closest_coin(point{0, 1}, coins) == point{0, 2});
+  REQUIRE(::closest_coin(point{0, 1}, coins) == point{0, 2});
 }
