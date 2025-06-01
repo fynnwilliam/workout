53113d667a41cb6d5513498f7ce5778c498ec1c0
fynnwilliam <fynnwilliam@gmail.com>
2025-06-01 22:58:28 +0100

    refactor: remove unused methods

--- linear_search.cpp
+++ linear_search.cpp
@@ -12,22 +12,6 @@ int linear_search(T elem, T const* v, int const& size) noexcept {
   return -1;
 }
 
-template <typename T> void print(T const& v) noexcept {
-  std::cout << '[';
-
-  for (auto const& item : v)
-    std::cout << item << ' ';
-
-  std::cout << "]\n";
-}
-
-void verify(int const& index) noexcept {
-  if (index == -1)
-    std::cout << "element not found\n";
-  else
-    std::cout << "element found at index " << index << std::endl;
-}
-
 TEST_CASE("linear_search") {
   int arr[]{3, 4, 33, 9, 23, 99};
   const int size = sizeof arr / sizeof arr[0];
