4e28c86f660d8ff76c78835759371f2ccdecbbd0
fynnwilliam <fynnwilliam@gmail.com>
2025-06-01 23:07:29 +0100

    test: add benchmarking
    runs in 406.53 ns

--- linear_search.cpp
+++ linear_search.cpp
@@ -1,3 +1,4 @@
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 template <typename T>
@@ -18,4 +19,14 @@ TEST_CASE("linear_search") {
   REQUIRE(linear_search(4, arr, size) == 1);
   REQUIRE(linear_search(9, arr, size) == 3);
   REQUIRE(linear_search(1, arr, size) == -1);
+}
+
+TEST_CASE("linear_search", "[!benchmark]") {
+  int arr[1'000]{3, 4, 33, 9, 23, 99};
+  arr[989] = 42;
+  const int size = sizeof arr / sizeof arr[0];
+
+  BENCHMARK("linear_search(42, arr, size)") {
+    return linear_search(42, arr, size);
+  };
 }
