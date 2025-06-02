d5d035f2ff897776b3dc72c39468f963b97e72f4
fynnwilliam <fynnwilliam@gmail.com>
2025-06-02 19:27:41 +0100

    feat: implement exercise for C++ Weekly - Ep 483
    ref: https://youtu.be/kjogmOXkipw?t=317

---
+++ random_bytes.cpp
@@ -0,0 +1,26 @@
+#include <bit>
+#include <random>
+
+struct usr_data {
+    char name[13]{};
+    char location[15]{};
+    unsigned age = 0;
+};
+
+int main() {
+    std::uint8_t data[sizeof(usr_data)];
+    std::mt19937 gen{std::random_device{}()};
+    std::uniform_int_distribution<> dist{0, UINT8_MAX};
+
+    for (auto& d : data) {
+        d = dist(gen);
+    }
+
+    auto info = std::bit_cast<usr_data>(data);
+
+    std::puts(info.name);
+    std::puts(info.location);
+    printf("%d\n", info.age);
+
+    return sizeof usr_data{};
+}
