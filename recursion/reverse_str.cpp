995cc65eab84425147ab4f7f36ed8458eae94aef
fynnwilliam <fynnwilliam@gmail.com>
2025-06-05 19:46:10 +0100

    test: add benchmarking
    runs in 7.20 ms

--- recursion/reverse_str.cpp
+++ recursion/reverse_str.cpp
@@ -1,3 +1,4 @@
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <string>
 
@@ -11,4 +12,11 @@ TEST_CASE("reverse_str") {
   REQUIRE(reverse_str("require") == "eriuqer"s);
   REQUIRE(reverse_str("reverse") == "esrever"s);
   REQUIRE(reverse_str("etarelecca") == "accelerate"s);
+}
+
+TEST_CASE("reverse_str", "[!benchmark]") {
+  std::string s(10'000, ' ');
+  BENCHMARK("reverse_str(s)") {
+    return reverse_str(s);
+  };
 }
