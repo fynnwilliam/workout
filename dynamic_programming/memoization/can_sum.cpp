2d062ecc78669608fa78ef3cc7720bdb6b726293
fynnwilliam <fynnwilliam@gmail.com>
2025-06-07 18:42:55 +0100

    test: add more assertions

--- dynamic_programming/memoization/can_sum.cpp
+++ dynamic_programming/memoization/can_sum.cpp
@@ -23,10 +23,12 @@ bool can_sum(
 }
 
 TEST_CASE("can_sum") {
-  std::unordered_map<int, bool> a, b, c, d, e;
+  std::unordered_map<int, bool> a, b, c, d, e, f, g;
   REQUIRE(can_sum(7, std::vector<int>{2, 3}, a) == true);
   REQUIRE(can_sum(7, std::vector<int>{5, 3, 4, 7}, b) == true);
   REQUIRE(can_sum(7, std::vector<int>{2, 4}, c) == false);
   REQUIRE(can_sum(19, std::vector<int>{12, 3, 5}, d) == true);
   REQUIRE(can_sum(41, std::vector<int>{7, 15}, e) == false);
+  REQUIRE(can_sum(8, std::vector<int>{2, 3, 5}, f) == true);
+  REQUIRE(can_sum(300, std::vector<int>{7, 14}, g) == false);
 }
