43ff3abc7db4b9ca5a1270582d596fc1d15188fd
fynnwilliam <fynnwilliam@gmail.com>
2025-06-08 23:05:09 +0100

    perf: use std::vector for caching
    significant improvement - runs in 2.06 ns

--- dynamic_programming/memoization/can_sum.cpp
+++ dynamic_programming/memoization/can_sum.cpp
@@ -3,38 +3,41 @@
 #include <unordered_map>
 #include <vector>
 
-bool can_sum(
-    int target, std::vector<int> const& v, std::unordered_map<int, bool>& m
+std::uint8_t can_sum(
+    int target, const std::vector<int>& v,
+    std::vector<std::uint8_t>& m
 ) {
-  if (m.count(target))
-    return m[target];
-  if (!target)
-    return true;
   if (target < 0)
-    return false;
+    return 0;
+  if (target == 0)
+    return 1;
+  if (target >= m.size())
+    m.resize(target + 1, 2);
+  else if (m[target] < 2)
+    return m[target];
 
-  for (int elem : v) {
-    if (can_sum(target - elem, v, m)) {
-      return m[target] = true;
+  for (const auto& elem : v) {
+    if (can_sum(target - elem, v, m) == 1) {
+      return m[target] = 1;
     }
   }
 
-  return m[target] = false;
+  return m[target] = 0;
 }
 
 TEST_CASE("can_sum") {
-  std::unordered_map<int, bool> a, b, c, d, e, f, g;
-  REQUIRE(can_sum(7, std::vector<int>{2, 3}, a) == true);
-  REQUIRE(can_sum(7, std::vector<int>{5, 3, 4, 7}, b) == true);
-  REQUIRE(can_sum(7, std::vector<int>{2, 4}, c) == false);
-  REQUIRE(can_sum(19, std::vector<int>{12, 3, 5}, d) == true);
-  REQUIRE(can_sum(41, std::vector<int>{7, 15}, e) == false);
-  REQUIRE(can_sum(8, std::vector<int>{2, 3, 5}, f) == true);
-  REQUIRE(can_sum(300, std::vector<int>{7, 14}, g) == false);
+  std::vector<std::uint8_t> a, b, c, d, e, f, g;
+  REQUIRE(can_sum(7, std::vector<int>{2, 3}, a) == 1);
+  REQUIRE(can_sum(7, std::vector<int>{5, 3, 4, 7}, b) == 1);
+  REQUIRE(can_sum(7, std::vector<int>{2, 4}, c) == 0);
+  REQUIRE(can_sum(19, std::vector<int>{12, 3, 5}, d) == 1);
+  REQUIRE(can_sum(41, std::vector<int>{7, 15}, e) == 0);
+  REQUIRE(can_sum(8, std::vector<int>{2, 3, 5}, f) == 1);
+  REQUIRE(can_sum(300, std::vector<int>{7, 14}, g) == 0);
 }
 
 TEST_CASE("can_sum", "[!benchmark]") {
-  std::unordered_map<int, bool> m;
+  std::vector<std::uint8_t> m;
   std::vector v{7, 14};
   BENCHMARK("can_sum(300, v, m)") {
     return can_sum(300, v, m);
