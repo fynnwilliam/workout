7f347ffa9261ab4b1b831072264b25b1c55748b6
fynnwilliam <fynnwilliam@gmail.com>
2025-06-09 22:36:12 +0100

    refactor: return a bool from can_sum

--- dynamic_programming/memoization/can_sum.cpp
+++ dynamic_programming/memoization/can_sum.cpp
@@ -2,37 +2,40 @@
 #include <catch2/catch_test_macros.hpp>
 #include <vector>
 
-std::uint8_t
-can_sum(int target, const std::vector<int>& v, std::vector<std::uint8_t>& m) {
+bool can_sum(
+    int target, const std::vector<int>& v, std::vector<std::uint8_t>& m
+) {
   if (target < 0)
-    return 0;
+    return false;
   if (target == 0)
-    return 1;
+    return true;
 
   const auto& target_ = static_cast<std::size_t>(target);
   if (target_ >= m.size())
     m.resize(target_ + 1, 2);
   else if (m[target_] < 2)
-    return m[target_];
+    return m[target_] == 1;
 
   for (const auto& elem : v) {
-    if (can_sum(target - elem, v, m) == 1) {
-      return m[target_] = 1;
+    if (can_sum(target - elem, v, m)) {
+      m[target_] = 1;
+      return true;
     }
   }
 
-  return m[target_] = 0;
+  m[target_] = 0;
+  return false;
 }
 
 TEST_CASE("can_sum") {
   std::vector<std::uint8_t> a, b, c, d, e, f, g;
-  REQUIRE(can_sum(7, std::vector<int>{2, 3}, a) == 1);
-  REQUIRE(can_sum(7, std::vector<int>{5, 3, 4, 7}, b) == 1);
-  REQUIRE(can_sum(7, std::vector<int>{2, 4}, c) == 0);
-  REQUIRE(can_sum(19, std::vector<int>{12, 3, 5}, d) == 1);
-  REQUIRE(can_sum(41, std::vector<int>{7, 15}, e) == 0);
-  REQUIRE(can_sum(8, std::vector<int>{2, 3, 5}, f) == 1);
-  REQUIRE(can_sum(300, std::vector<int>{7, 14}, g) == 0);
+  REQUIRE(can_sum(7, std::vector<int>{2, 3}, a) == true);
+  REQUIRE(can_sum(7, std::vector<int>{5, 3, 4, 7}, b) == true);
+  REQUIRE(can_sum(7, std::vector<int>{2, 4}, c) == false);
+  REQUIRE(can_sum(19, std::vector<int>{12, 3, 5}, d) == true);
+  REQUIRE(can_sum(41, std::vector<int>{7, 15}, e) == false);
+  REQUIRE(can_sum(8, std::vector<int>{2, 3, 5}, f) == true);
+  REQUIRE(can_sum(300, std::vector<int>{7, 14}, g) == false);
 }
 
 TEST_CASE("can_sum", "[!benchmark]") {
