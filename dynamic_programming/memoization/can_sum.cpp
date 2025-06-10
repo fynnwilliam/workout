219bb51331248edcf204bef517b097078e4e70a4
fynnwilliam <fynnwilliam@gmail.com>
2025-06-10 22:54:37 +0100

    refactor: contain m with a lambda capture
    helps reduce cache creation on several calls to can_sum

--- dynamic_programming/memoization/can_sum.cpp
+++ dynamic_programming/memoization/can_sum.cpp
@@ -2,30 +2,29 @@
 #include <catch2/catch_test_macros.hpp>
 #include <vector>
 
-bool can_sum(
-    int target, const std::vector<int>& v, std::vector<std::uint8_t>& m
-) {
-  if (target < 0)
-    return false;
-  if (target == 0)
-    return true;
+auto can_sum = [m = std::vector<std::uint8_t>{}](
+                   int target, const std::vector<int>& v
+               ) mutable {
+  m.resize(static_cast<std::size_t>(target), 2);
+  return [&m](this auto&& can_sum_, int t, const auto& v_) -> std::uint8_t {
+    if (t < 0)
+      return 0;
+    if (t == 0)
+      return 1;
 
-  const auto& target_ = static_cast<std::size_t>(target);
-  if (target_ >= m.size())
-    m.resize(target_ + 1, 2);
-  else if (m[target_] < 2)
-    return m[target_] == 1;
+    const auto& target_ = static_cast<std::size_t>(t);
+    if (m[target_] < 2)
+      return m[target_];
 
-  for (const auto& elem : v) {
-    if (can_sum(target - elem, v, m)) {
-      m[target_] = 1;
-      return true;
+    for (const auto& elem : v_) {
+      if (can_sum_(t - elem, v_) == 1) {
+        return m[target_] = 1;
+      }
     }
-  }
 
-  m[target_] = 0;
-  return false;
-}
+    return m[target_] = 0;
+  }(target, v) == 1;
+};
 
 TEST_CASE("can_sum") {
   std::vector<std::uint8_t> a, b, c, d, e, f, g;
