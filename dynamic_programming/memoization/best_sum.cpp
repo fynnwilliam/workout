915dc66546fc1dec15c22fee8069ed61856f711b
fynnwilliam <fynnwilliam@gmail.com>
2025-06-12 20:25:58 +0100

    test: add more assertions
    some assertions are currently failling - will resolve them

--- dynamic_programming/memoization/best_sum.cpp
+++ dynamic_programming/memoization/best_sum.cpp
@@ -31,4 +31,7 @@ std::unique_ptr<std::vector<int>> best_sum(
 
 TEST_CASE("best_sum") {
   REQUIRE(*best_sum(8, {1, 2, 4, 25}) == std::vector{4, 4});
+  REQUIRE(*best_sum(7, {5, 3, 4, 7}) == std::vector{7});
+  REQUIRE(*best_sum(8, {2, 3, 5}) == std::vector{3, 5});
+  REQUIRE(*best_sum(100, {1, 2, 5, 25}) == std::vector{25, 25, 25, 25});
 }
