625187d46dc16849e56cc43f782b9dba41f4c8de
fynnwilliam <fynnwilliam@gmail.com>
2025-06-12 20:07:13 +0100

    style: apply .clang-format

--- dynamic_programming/memoization/best_sum.cpp
+++ dynamic_programming/memoization/best_sum.cpp
@@ -1,45 +1,44 @@
-#include <unordered_map>
 #include <iostream>
-#include <vector>
 #include <memory>
+#include <unordered_map>
+#include <vector>
 
-std::unique_ptr<std::vector<int>> best_sum(int target, std::vector<int> const& v, std::unordered_map<int, std::vector<int>>&& m = {})
-{
-    if (m.count(target)) return std::make_unique<std::vector<int>>(m[target]);
-    if (!target)         return std::make_unique<std::vector<int>>();
-    if (target < 0)      return nullptr;
+std::unique_ptr<std::vector<int>> best_sum(
+    int target, std::vector<int> const& v,
+    std::unordered_map<int, std::vector<int>>&& m = {}
+) {
+  if (m.count(target))
+    return std::make_unique<std::vector<int>>(m[target]);
+  if (!target)
+    return std::make_unique<std::vector<int>>();
+  if (target < 0)
+    return nullptr;
 
-    std::unique_ptr<std::vector<int>> shortest_c{};
-    
-    for (int elem : v)
-    {
-        if (auto current_c = best_sum(target - elem, v, std::move(m)); current_c)
-        {
-            current_c->push_back(elem);
-            if (!shortest_c || current_c->size() < shortest_c->size())
-            {
-                shortest_c = std::move(current_c);
-            }
-        }
-        
+  std::unique_ptr<std::vector<int>> shortest_c{};
+
+  for (int elem : v) {
+    if (auto current_c = best_sum(target - elem, v, std::move(m)); current_c) {
+      current_c->push_back(elem);
+      if (!shortest_c || current_c->size() < shortest_c->size()) {
+        shortest_c = std::move(current_c);
+      }
     }
+  }
 
-    m[target] = *shortest_c;
-    return shortest_c;
+  m[target] = *shortest_c;
+  return shortest_c;
 }
 
-int main()
-{
-    auto v{ best_sum(8, {1, 2, 4, 25}) };
-    
-    if (!v)
-    {
-        std::cout << "could not sum up...\n";
-        return 0;
-    }
-    
-    for (int elem : *v)
-        std::cout << elem << ' ';
+int main() {
+  auto v{best_sum(8, {1, 2, 4, 25})};
 
-    std::cout << std::endl;
+  if (!v) {
+    std::cout << "could not sum up...\n";
+    return 0;
+  }
+
+  for (int elem : *v)
+    std::cout << elem << ' ';
+
+  std::cout << std::endl;
 }
