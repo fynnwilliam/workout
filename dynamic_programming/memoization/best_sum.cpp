e204c64e57903ce01611ac812b6c1bd2e7562f4e
fynnwilliam <fynnwilliam@gmail.com>
2025-06-13 23:44:58 +0100

    test: update test assertions - no need dereferencing

--- dynamic_programming/memoization/best_sum.cpp
+++ dynamic_programming/memoization/best_sum.cpp
@@ -31,10 +31,10 @@ std::vector<int> best_sum(
 }
 
 TEST_CASE("best_sum") {
-  REQUIRE(*best_sum(8, {1, 2, 4, 25}) == std::vector{4, 4});
-  REQUIRE(*best_sum(7, {5, 3, 4, 7}) == std::vector{7});
-  REQUIRE(*best_sum(8, {2, 3, 5}) == std::vector{5, 3});
-  REQUIRE(*best_sum(100, {1, 2, 5, 25}) == std::vector{25, 25, 25, 25});
+  REQUIRE(best_sum(8, {1, 2, 4, 25}) == std::vector{4, 4});
+  REQUIRE(best_sum(7, {5, 3, 4, 7}) == std::vector{7});
+  REQUIRE(best_sum(8, {2, 3, 5}) == std::vector{5, 3});
+  REQUIRE(best_sum(100, {1, 2, 5, 25}) == std::vector{25, 25, 25, 25});
 }
 
 TEST_CASE("best_sum", "[!benchmark]") {
