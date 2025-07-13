8dbeda912e121ef08cf9281a985375b18d63b4ce
fynnwilliam <fynnwilliam@gmail.com>
2025-07-13 21:42:13 +0100

    feat: implement Maximum Matching of Players With Trainers
    https://leetcode.com/problems/maximum-matching-of-players-with-trainers

---
+++ leetcode/match_players_and_trainers.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <vector>
+
+int match_players_and_trainers(
+    std::vector<int>& players, std::vector<int>& trainers
+) {
+  std::ranges::sort(players);
+  std::ranges::sort(trainers);
+
+  auto p_first = cbegin(players);
+  auto p_end = cend(players);
+  auto t_first = cbegin(trainers);
+  auto t_end = cend(trainers);
+  int matches = 0;
+  while (p_first < p_end && t_first < t_end) {
+    if (*p_first <= *t_first) {
+      ++p_first;
+      ++t_first;
+    } else
+      ++p_first;
+  }
+
+  return matches;
+}
