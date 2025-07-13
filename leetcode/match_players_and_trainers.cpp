59af75b6792b55e7cbf0a81266e9200f89300f3c
fynnwilliam <fynnwilliam@gmail.com>
2025-07-13 22:08:54 +0100

    test: add benchmarking
    runs in ~41 ns

--- leetcode/match_players_and_trainers.cpp
+++ leetcode/match_players_and_trainers.cpp
@@ -1,5 +1,6 @@
-#include <catch2/catch_test_macros.hpp>
 #include <algorithm>
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include <vector>
 
 int match_players_and_trainers(
@@ -26,7 +27,16 @@ int match_players_and_trainers(
 }
 
 TEST_CASE("match_players_and_trainers") {
-    std::vector players{4, 7, 9};
-    std::vector trainers{8, 2, 5, 8};
-    REQUIRE(match_players_and_trainers(players, trainers) == 2);
+  std::vector players{4, 7, 9};
+  std::vector trainers{8, 2, 5, 8};
+  REQUIRE(match_players_and_trainers(players, trainers) == 2);
+}
+
+TEST_CASE("match_players_and_trainers", "[!benchmark]") {
+  std::vector players{4, 7, 9, 9, 2, 4, 10, 32, 9, 14};
+  std::vector trainers{8, 2, 5, 8, 3, 4, 13, 29, 22, 7};
+
+  BENCHMARK("match_players_and_trainers(players, trainers)") {
+    return match_players_and_trainers(players, trainers);
+  };
 }
