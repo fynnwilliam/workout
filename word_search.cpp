4da86ba57b573d53f4eb4ef310728be56429371e
fynnwilliam <fynnwilliam@gmail.com>
2025-08-23 14:25:18 +0100

    perf: simplify computation for next row
    executing r * columns is not required

--- word_search.cpp
+++ word_search.cpp
@@ -5,12 +5,12 @@ bool horizontal_search(
     std::unique_ptr<const char*>& puzzle_ptr, std::uint32_t rows,
     std::uint32_t columns, std::string_view word
 ) {
-  const auto& head = *puzzle_ptr.get();
+  auto head = *puzzle_ptr.get();
   for (std::uint32_t r = 0u; r < rows; ++r) {
-    const auto start = r * columns;
-    std::string_view row_view{head + start, columns};
+    std::string_view row_view{head, columns};
     if (row_view.contains(word))
       return true;
+    head += columns;
   }
 
   return false;
