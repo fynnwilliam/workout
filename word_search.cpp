b042826cae6db30936fbbe9d4468ea23b1461230
fynnwilliam <fynnwilliam@gmail.com>
2025-08-24 20:24:35 +0100

    refactor: explicitly mark column_ptr as const char*
    previous declaration yielded a char* const

--- word_search.cpp
+++ word_search.cpp
@@ -70,13 +70,13 @@ TEST_CASE("horizontal_search", "[!benchmark]") {
 }
 
 TEST_CASE("vertical_search") {
-  const auto column_ptr = &puzzle[0][0];
+  const char* column_ptr = &puzzle[0][0];
   REQUIRE(vertical_search(column_ptr, 10, 10, "000") == false);
   REQUIRE(vertical_search(column_ptr, 10, 10, "ooo") == true);
 }
 
 TEST_CASE("vertical_search", "[!benchmark]") {
-  const auto column_ptr = &puzzle[0][0];
+  const char* column_ptr = &puzzle[0][0];
   BENCHMARK("vertical_search(column_ptr, 10, 10, ooo) == true)") {
     return vertical_search(column_ptr, 10, 10, "ooo");
   };
