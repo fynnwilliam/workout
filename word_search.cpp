ee66e793ca05a31a57c5c15e89bb8a6fbd931926
fynnwilliam <fynnwilliam@gmail.com>
2025-08-29 18:16:04 +0100

    test: add asserstion for searching the anti-slope

--- word_search.cpp
+++ word_search.cpp
@@ -116,11 +116,11 @@ namespace {
 std::array<std::array<char, 10>, 10> puzzle{
     std::array{'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
     std::array{'x', 'x', 'e', 's', 'r', 'e', 'v', 'e', 'r', 'h'},
-    std::array{'x', 'x', 'x', 'x', 'x', 's', 'x', 'x', 'x', 'v'},
-    std::array{'x', 'x', 'x', 'x', 'x', 'r', 'x', 'x', 'x', 'e'},
-    std::array{'s', 'x', 'x', 'x', 'x', 'e', 'x', 'x', 'x', 'r'},
+    std::array{'x', 'x', 'x', 'x', 't', 's', 'x', 'x', 'x', 'v'},
+    std::array{'x', 'x', 'x', 'n', 'x', 'r', 'x', 'x', 'x', 'e'},
+    std::array{'s', 'x', 'a', 'x', 'x', 'e', 'x', 'x', 'x', 'r'},
     std::array{'x', 'l', 'x', 'x', 'x', 'v', 'x', 'x', 'x', 't'},
-    std::array{'x', 'x', 'o', 'x', 'x', 'e', 'x', 'x', 'x', 'i'},
+    std::array{'s', 'x', 'o', 'x', 'x', 'e', 'x', 'x', 'x', 'i'},
     std::array{'x', 'x', 'x', 'p', 'x', 'r', 'x', 'x', 'x', 'c'},
     std::array{'x', 'x', 'x', 'x', 'e', 'v', 'x', 'x', 'x', 'a'},
     std::array{'h', 'o', 'r', 'i', 'z', 'o', 'n', 't', 'a', 'l'}
@@ -159,6 +159,7 @@ TEST_CASE("slope_search") {
   const char* slope_ptr = &puzzle[0][0];
   REQUIRE(slope_search(slope_ptr, 10, 10, "000") == false);
   REQUIRE(slope_search(slope_ptr, 10, 10, "slope") == true);
+  REQUIRE(slope_search(slope_ptr, 10, 10, "slant") == true);
 }
 
 TEST_CASE("slope_search", "[!benchmark]") {
